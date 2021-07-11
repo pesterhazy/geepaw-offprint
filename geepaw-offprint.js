800a0a1e7133abd577683c566f46e640a097c93a
Paulus Esterhazy <pesterhazy@gmail.com>
2021-07-11 11:09:55 +0200

    WIP

--- geepaw-offprint.js
+++ geepaw-offprint.js
@@ -1,17 +1,30 @@
-const puppeteer = require('puppeteer')
-const fsPromises = require('fs').promises;
+const puppeteer = require('puppeteer');
+const fs = require('fs').promises;
+
+
  
-async function printPDF() {
+async function printPDF(url, to) {
   const browser = await puppeteer.launch({ headless: true });
   const page = await browser.newPage();
-  const url = "https://www.geepawhill.org/2020/07/21/understanding-incremental-switchover/";
   await page.goto(url, {waitUntil: 'networkidle0'});
   await page.addScriptTag({content: `let el= document.querySelector("main"); document.body.innerHTML=""; document.body.appendChild(el)`});
   await page.addStyleTag({content: `form {display: none;}`});
   const pdf = await page.pdf({ format: 'A4' });
  
   await browser.close();
-  await fsPromises.writeFile("out.pdf", pdf);
+  await fs.writeFile(to, pdf);
 }
 
-printPDF();
+async function main() {
+  const urls = (await fs.readFile("urls.txt", "utf8")).split(/\n/).filter(s => s);
+
+  for (const [idx, url] of urls.entries()) {
+    const to = `out/${idx}.pdf`;
+    console.log(url);
+    await printPDF(url, to);
+    console.log(to);
+  }
+  process.exit(0);
+};
+
+main();
